6499a1b9eafd6a18d8edcbb66f93461b1d0a8fd8
DevanTurtle7 <devankav7@gmail.com>
2025-11-15 18:52:50 -0500

    Get all photos

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -102,6 +102,243 @@ const projects: Project[] = [
   },
 ]
 
+export interface Photo {
+  src: string
+}
+
+export interface PhotoProject {
+  title: string;
+  photos: Photo[]
+}
+
+const photos: PhotoProject[] = [
+  {
+    title: "CGI Composite",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/cgi-composite/GoodOmen.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/cgi-composite/kitties-22.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/cgi-composite/kitties-60.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/cgi-composite/under-ice.JPEG",
+      },
+    ],
+  },
+  {
+    title: "Commercial",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-138.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-154.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-160.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-203.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-53.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-56.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-60.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/grad-pic-88.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/headshot-129.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/headshot-132.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/headshot-37.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/commercial/headshot-65.jpg",
+      },
+    ],
+  },
+  {
+    title: "Costa Rica",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/costa-rica/_DSC0401.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/costa-rica/01.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/costa-rica/reflection1.jpg",
+      },
+    ],
+  },
+  {
+    title: "Fashion",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/color_1_3.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/highkey_1_3.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/highkey_full_length.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/lights_off_3_4.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/lights_off_full_body.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/stop1_1_3.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/catalogue-R13/stop1_full_body.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/LUCY_Future_Fashion_1-edit.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/Lucy_Future_Fashion_CU.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/reflection1-11.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/reflection1-4.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/reflection1-7.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/future-of-fashion/reflection1.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/Lucy_Strobe-as-fill.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-142.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-239.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-401.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-411.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-58.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-89.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-95.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/fashion/pjs-shoot/Editsbedshoot-98.jpg",
+      },
+    ],
+  },
+  {
+    title: "Freestyle",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0292.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0319.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0397.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0584.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0586-2.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0596.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0681-2.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0693.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0698-2.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/5N5A0753-2.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/maggiesdigicam-27.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/freestyle/maggiesdigicam-66.jpg",
+      },
+    ],
+  },
+  {
+    title: "The Patrol",
+    photos: [
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC1012.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC1041.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC1943.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC1975.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC1992.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC2010.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC2018.jpg",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC2055.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/_DSC2059.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/Bike_Edit.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/cu-site-side.png",
+      },
+      {
+        src: "https://d3amd0zp63qrni.cloudfront.net/assets/images/photo/patrol/IMG_6923.PNG",
+      },
+    ]
+  },
+]
+
 
 function App() {
   const [loadedProjects, setLoadedProjects] = useState<any>([]);
@@ -133,7 +370,7 @@ function App() {
         <Route path="/reel" element={<Reel/>} />
         <Route path="/video" element={<Work projects={projects}/>} />
         <Route path="/contact" element={<Contact/>} />
-        <Route path="/photo" element={<Photo/>} />
+        <Route path="/photo" element={<Photo photos={photos}/>} />
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/photo.tsx
+++ frontend/src/pages/photo.tsx
@@ -1,10 +1,24 @@
+import type { PhotoProject } from "src/main"
 import Footer from "../components/Footer/Footer"
 import TitleBar from "../components/TitleBar/TitleBar"
 
-export default function Photo() {
+import "../styles/photo.css"
+
+export default function Photo({ photos } : { photos: PhotoProject[] }) {
+  const elements: any[] = [];
+
+  photos.forEach((project) => {
+    elements.push(<p>{project.title}</p>);
+    project.photos.forEach(({src}) => {
+      elements.push(<img src={src}/>)
+    })
+  })
+
+
   return (
     <div>
       <TitleBar route="photo"/>
+      {elements}
       <Footer/>
     </div>
   )
